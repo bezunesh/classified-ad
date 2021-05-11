100852f71317e65d3aeaf0bc86480d5ceec67f4e
Bezunesh <zueb21@gmail.com>
2021-05-11 17:10:01 -0400

    used generic class based views

--- ad_rest_api/views.py
+++ ad_rest_api/views.py
@@ -1,39 +1,18 @@
-from rest_framework import mixins
 from rest_framework import generics
 from ad.models import Post
 from ad_rest_api.serializers import AdSerializer
 
 
-class PostList(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView ):
+class PostList(generics.ListCreateAPIView):
     '''
     List all ad posts, or create a new post.
     '''
     queryset = Post.objects.all()
     serializer_class = AdSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class PostDetail(mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 generics.GenericAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     '''
     retrive, update or delete a post
     '''
     queryset = Post.objects.all()
     serializer_class = AdSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
