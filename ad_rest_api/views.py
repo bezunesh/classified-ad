4661dd3fbffd8b71d582ed4eb673f664775e8688
Bezunesh <zueb21@gmail.com>
2021-05-11 16:37:09 -0400

    changed function based views to class based views

--- ad_rest_api/views.py
+++ ad_rest_api/views.py
@@ -1,49 +1,52 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+#from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404
 from ad.models import Post
 from ad_rest_api.serializers import AdSerializer
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def post_list(request, format=None):
+
+class PostList(APIView):
     '''
     List all ad posts, or create a new post.
     '''
-    if request.method == 'GET':
+    def get(self, request, format=None):
         posts = Post.objects.all()
         serializer = AdSerializer(posts, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = AdSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def post_detail(request, pk, format=None):
+
+class PostDetail(APIView):
     '''
     retrive, update or delete a post
     '''
-    try:
-        post = Post.objects.get(pk=pk)
-    except Post.DoesNotExist:
-        return Response(status=status.HTTP_400_NOT_FOUND)
-    
-    if request.method == 'GET':
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk, format=None):
+        post = self.get_object(pk)
         serializer = AdSerializer(post)
         return Response(serializer.data)
     
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        post = self.get_object(pk)
         serializer = AdSerializer(post, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        post = self.get_object(pk)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
